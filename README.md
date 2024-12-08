9c3e24579dd6789d1524a9d858580d80d8f5f8bc
zhabiba24 <182411820+zhabiba24@users.noreply.github.com>
2024-12-08 16:10:54 +0000

    feat: btrix embed working (#5)

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-hello
+# Overview
+
+This is the frontend for the Sudan Digital Archive project.
+
+## Local Development
+
+Clone the repo, install `https://pnpm.io/`, run `pnpm i` and then
+`pnpm run dev`. 
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import About from "./pages/about.tsx";
 import Archive from "./pages/archive.tsx";
+import BrowsertrixDemo from "./pages/browsertrixDemo.tsx";
 import "./il18n.ts";
 import "./styles.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -23,12 +24,33 @@ const router = createBrowserRouter([
     element: <Archive />,
   },
   {
+    path: "/browsertrix-demo",
+    element: <BrowsertrixDemo />,
+  },
+  {
     path: "*",
     element: <Home />,
   },
 ]);
-
+// this service worker is seriously cool
+// it lives in public/replay/sw.js
+// and imports a service worker that webrecorder built
+// it intercepts all requests to /replay/sw.js to allow
+// use of a <replay-web-page/> component that lets you embed
+// full browser crawls in a webpage using JS injected
+// with a script tag in `index.html`
+// https://github.com/webrecorder/replayweb.page
+navigator.serviceWorker
+.register("/replay/sw.js")
+.then((registration) =>
+  console.log(
+    "Service Worker registration successful with scope: ",
+    registration.scope
+  )
+)
+.catch((err) => console.log("Service Worker registration failed: ", err));
 createRoot(document.getElementById("root")!).render(
+  
   <StrictMode>
     <ColorModeScript initialColorMode="dark" />
     <ChakraThemeRTLProvider>
