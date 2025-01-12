74b487edc47b372eb745622f351ba0c6dbd37733
Zainab Habiba <182411820+zhabiba24@users.noreply.github.com>
2025-01-12 12:08:18 +0000

    feat: save where i got to on btrix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+import * as React from 'react'
+
+interface ReplayWebPageProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+  source: string,
+  url: string,
+}
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'replay-web-page': ReplayWebPageProps;
+        }
+    }
+}
