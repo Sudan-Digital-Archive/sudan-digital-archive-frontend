f398ca16aff7656784c933fac2a2c7fa4e346f3c
Zainab Habiba <182411820+zhabiba24@users.noreply.github.com>
2025-02-11 00:09:26 +0000

    feat: contact and some tweaks

--- src/components/date_picker.tsx
+++ src/components/date_picker.tsx
@@ -7,20 +7,16 @@ import { useTranslation } from "react-i18next";
 interface ArchiveDatePickerProps {
   selected: Date | null;
   onChange: (date: Date | null) => void;
-  isClearable?: boolean;
 }
 export function ArchiveDatePicker({
-  isClearable = true,
   onChange,
   selected,
 }: ArchiveDatePickerProps) {
-  const { i18n} = useTranslation();
+  const { i18n } = useTranslation();
   return (
     <Box className="dark-theme" mr={2} ml={2}>
       <DatePicker
         dateFormat={i18n.language === "ar" ? "yyyy/MM/dd" : "MM/dd/yyyy"}
-        // doesn't work with right to left sad face
-        isClearable={i18n.language === "ar" ? false: isClearable}
         selected={selected}
         onChange={onChange}
         showYearDropdown
