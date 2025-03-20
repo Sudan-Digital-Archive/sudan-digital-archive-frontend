ee5d8bedf0cefa942dcb953b3e2a435ba7b0b14a
Zainab Habiba <182411820+zhabiba24@users.noreply.github.com>
2025-03-20 20:25:50 +0000

    feat: wip on replay web page change

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-const apiURL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api/v1/"
-    : "https://octopus-app-58s4l.ondigitalocean.app/sda-api/api/v1/";
+const apiURL ="https://octopus-app-58s4l.ondigitalocean.app/sda-api/api/v1/"
+  // import.meta.env.MODE === "development"
+  //   ? "http://localhost:5000/api/v1/"
+  //   : "https://octopus-app-58s4l.ondigitalocean.app/sda-api/api/v1/";
 export const appConfig = {
   apiURL: apiURL,
 };
