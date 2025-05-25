27cf4a3f94f2828cc201893b33913fea63798dd6
zhabiba24 <182411820+zhabiba24@users.noreply.github.com>
2025-05-25 15:55:48 +0100

    Fix cookie domain auth (#44)

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 const apiURL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api/v1/"
-    : "https://octopus-app-58s4l.ondigitalocean.app/sda-api/api/v1/";
+    : "https://api.sudandigitalarchive.com/sda-api/api/v1/";
 
 const appURLFrontend =
   import.meta.env.MODE === "development"
