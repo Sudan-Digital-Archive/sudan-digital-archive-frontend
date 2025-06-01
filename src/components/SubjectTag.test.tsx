90b9c5cb32dd3ca77ceb0750ab1f79c0c08d5d74
Zainab Habiba <182411820+zhabiba24@users.noreply.github.com>
2025-06-01 11:48:02 +0100

    fix: correct test

--- src/components/SubjectTag.test.tsx
+++ src/components/SubjectTag.test.tsx
@@ -1,31 +1,11 @@
 import { SubjectTag } from "./SubjectTag.tsx";
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect } from "vitest";
 import { renderWithProviders } from "../../testUtils/testHelpers.tsx";
-import { screen, fireEvent } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
 describe("SubjectTag", () => {
   it("should render properly with provided text", () => {
     renderWithProviders(<SubjectTag label="Politics"></SubjectTag>);
     expect(screen.getByText("Politics")).toBeTruthy();
-  });
-
-  it("should not display a close button by default", () => {
-    renderWithProviders(<SubjectTag label="Politics" />);
-    expect(screen.queryByRole("button")).toBeNull();
-  });
-
-  it("should display a close button when hasCloseButton is true", () => {
-    renderWithProviders(<SubjectTag label="Politics" hasCloseButton />);
-    expect(screen.getByRole("button")).toBeTruthy();
-  });
-
-  it("should call onClose callback when close button is clicked", () => {
-    const onCloseMock = vi.fn();
-    renderWithProviders(
-      <SubjectTag label="Politics" hasCloseButton onClose={onCloseMock} />
-    );
-
-    fireEvent.click(screen.getByRole("button"));
-    expect(onCloseMock).toHaveBeenCalledTimes(1);
   });
 });
